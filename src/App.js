ab1306d74b54ebd228579d42affd0f110684eda5
pogikeith <pogikeith@yahoo.com>
2019-10-14 11:19:52 -0600

    front

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import TodoItem from './Todoitem'
+
+import './index.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      todos: [],
+      todo: ""
+   }
+}
+
+componentDidMount() {
+  fetch("http://localhost:5000/todos")
+    .then(response => response.json())
+    .then(data => this.setState ({ todos: data}))
+}
+
+renderTodos = () => {
+  return this.state.todos.map(todo => {
+      return (
+      <TodoItem 
+        key={todo.id}
+        title={todo.title} 
+        done={todo.done}
+        id={todo.id} 
+        delete={this.deleteTodo}
+     />
+   )
+ })
+}
+
+handleChange = event => {
+  this.setState({todo: event.target.value})
+}
+
+addTodo = event => {
+  event.preventDefault()
+  fetch("http://localhost:5000/todo" , {
+    method: "POST" ,
+    headers: {"content-type" : "application/json"},
+    body: JSON.stringify({
+      title: this.state.todo,
+      done: false
+    })
+  })
+    .then(response => response.json())
+    .then(data => 
+        this.setState({
+      todos: [...this.state.todos, data],
+      todo: ""
+   })
+  )
+}
+
+deleteTodo = id => {
+  fetch("http://localhost:5000/todo/$(id)`" , {
+    method: "DELETE"
+  }).then(
+      this.setState({ 
+         todos: this.state.todos.filter(todo => todo.id !== id)
+      })
+  )
+}
+
+render() {
+    return (
+      <div className="App">
+        <h1>ToDo List</h1> 
+        <form onSubmit={this.addTodo}> 
+            <input type= 'text' placeholder='Add Todo' value={this.state.todo} onChange={this.handleChange}/> 
+            <button type= 'submit' >Add</button> 
+        </form>
+        {this.renderTodos()}
+      </div>
+    )
+ } 
+}
+export default App;
---
+++ src/Todoitem.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+
+
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            done: props.done
+        }
+    }
+
+    toggleDone = () => {
+      fetch(`http://localhost:5000/todo/${this.props.id}` , {
+        method: "PUT" , 
+        headers: { "content-type": "application/json"} ,
+        body: JSON.stringify({
+            title: this.props.title, 
+            done: !this.state.done 
+        })
+    }).then(this.setState({ done: !this.state.done }))    
+  } 
+
+    render() {
+        return (
+            <div className= 'todo-item'>
+                <input 
+                type= 'checkbox' 
+                onChange= {this.toggleDone}
+                />
+                <p className= {this.state.done && 'done'}> {this.props.title}</p> 
+                <button onClick={() => this.props.delete(this.props.id)}>X</button>
+            </div> 
+        ) 
+    }
+}
+
+export default TodoItem; 
